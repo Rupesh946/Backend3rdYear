ec69d9164dc677646eaedaee1d69732ac66f7138
RupeshJi <rupeshgurjar946@gmail.com>
2025-07-13 22:53:49 +0530

    fhrjk

---
+++ file.mjs
@@ -0,0 +1,10 @@
+function add(a,b){
+    return a+b
+}
+function sub(a,b){
+    return a-b 
+}
+function multiply(a,b){
+    return a*b;
+}
+export default mult
---
+++ main.js
@@ -0,0 +1,5 @@
+let somesun= require("./Lec2");
+console.log(somefun);
+let res1=somefun.sum(2,3);
+let res2=somefun.sum(2,3);
+console.log(res1,res2)
