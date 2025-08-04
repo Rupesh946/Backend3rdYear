7e956eadeea2ab43ad779fad3834b10dc1465ba1
RupeshJi <rupeshgurjar946@gmail.com>
2025-08-04 15:31:57 +0530

    jdjn

---
+++ Lec13/index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const app=express();
+const fs=require('fs');
+
+app.get('/users',(req,res)=>{
+    fs.readFile("./users.json","utf-8",function(err,data){
+        if(err) res.send(err);
+        let allUsers=JSON.parse(data);
+        res.json(allUsers);
+    })
+})
+
+
+app.listen(3000,function(){
+    console.log("Server Started on Port 3000");
+});
